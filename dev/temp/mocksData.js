afb30a57c36a15dd66ffa3bc00c62a3ac0ad4f60
Sergey Veremiychuk <veremey4uk@gmail.com>
2019-02-13 11:26:58 +0200

    add gitignore

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,116 @@
+'head': {
+    defaults: {
+        title: 'Aero training',
+        useSocialMetaTags: false
+    }
+},
+
+'popular': {
+	pop: {
+		title: 'Popular videos',
+		item:[{
+			src: 'static/img/content/img-01.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-02.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-03.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-04.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-05.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-06.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			},
+			]
+		},
+	more: {
+		title: 'More courses by this Trainer',
+		item:[{
+			src: 'static/img/content/img-01.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-02.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-03.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-04.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-05.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			}, {
+			src: 'static/img/content/img-06.jpg',
+			title: 'Lorem Ipsum Dolores',
+			desc: 'Nam libero tempore, cum soluta',
+			category: 'category name',
+			time: '20 min'
+			},
+			]
+		}
+},
+
+'study': {},
+
+__pages: [{
+                name: 'add-video',
+                href: 'add-video.html'
+             },{
+                name: 'creating-course',
+                href: 'creating-course.html'
+             },{
+                name: 'creating-modules',
+                href: 'creating-modules.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'sertificate',
+                href: 'sertificate.html'
+             },{
+                name: 'singup',
+                href: 'singup.html'
+             },{
+                name: 'video',
+                href: 'video.html'
+             }]
